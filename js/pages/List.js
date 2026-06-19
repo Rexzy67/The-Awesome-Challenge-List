907cf870a72779271f15b21b6cc3ad807d93584a
Rexzy67 <rexzy62@proton.me>
2026-06-19 16:16:38 +0200

    fix: update submission requirements for FPS limit and video recording criteria

--- js/pages/List.js
+++ js/pages/List.js
@@ -151,19 +151,19 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 720fps)
                     </p>
                     <p>
                         Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        Have either source audio or clicks/taps in the video.
                     </p>
                     <p>
                         The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        The recording must also show the player hit the endwall and show the FULL Level Completion Screen, or the completion will be invalidated.
                     </p>
                     <p>
                         Do not use secret routes or bug routes
